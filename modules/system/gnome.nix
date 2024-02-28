97b9b9b492b4d0dac71afa616d152fe385abfb4f
Ayush Singh <ayushdevel1325@gmail.com>
2024-02-28 19:34:13 +0530

    system: gnome: Add dconf editor
    - Useful for querying and setting dconf values in nix config.
    
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.xserver.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    gnome.dconf-editor
+  ];
 }
